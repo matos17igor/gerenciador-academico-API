3f76a4a5e2d3133585401bd15329fa2a9518a56e
matos17igor <igorm17_pvp@hotmail.com>
2026-02-11 01:14:37 -0300

    cria rota de post user

--- src/app.js
+++ src/app.js
@@ -2,17 +2,11 @@ const express = require("express");
 const app = express();
 const db = require("./config/database");
 const PORT = 3000;
+const userRoutes = require("./routes/userRoutes");
 
 app.use(express.json());
 
-app.get("/teste", async (req, res) => {
-  try {
-    const [rows] = await db.query("SELECT 1 + 1 AS result");
-    res.json({ message: "Conectado ao MYSQL!", result: rows[0].result });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
+app.use("/users", userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
