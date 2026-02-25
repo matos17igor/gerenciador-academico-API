f145aaf70cbe48365802e699a5dca8a93c87c707
matos17igor <igorm17_pvp@hotmail.com>
2026-02-25 15:50:59 -0300

    adiciona funcao updtaTask

--- src/services/taskServices.js
+++ src/services/taskServices.js
@@ -23,4 +23,31 @@ async function listTasks(userId) {
   return await taskRepository.findAll(userId);
 }
 
-module.exports = { registerTask, listTasks };
+async function updateTask(id, userId, taskData) {
+  const { title, description, deadline } = taskData;
+
+  const updateData = new Date(deadline);
+  const now = new Date();
+
+  if (updateData < now) {
+    throw new Error("A data de entrega nao pode ser no passado!");
+  }
+
+  const result = await taskRepository.update({
+    title,
+    description,
+    deadline,
+    id,
+    userId,
+  });
+
+  if (result.affectedRows === 0) {
+    throw new Error(
+      "Tarefa nao encontrada ou voce nao tem permissao para edita-la"
+    );
+  }
+
+  return { message: "Tarefa atualizada com sucesso!" };
+}
+
+module.exports = { registerTask, listTasks, updateTask };
